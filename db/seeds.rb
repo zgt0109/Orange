6cf5a0c90c22f2a0681194f34d1901ab1204502d
guxiaobai <sikuan.gu@gmail.com>
2015-06-04 15:26:26 +0800

    devise for enterprise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Enterprise.create(email:'ent_01@10000zhi.cn', password: 'password')
