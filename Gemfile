49212bbf67bc734e5e8daa9fff9dcf12ce47d006
guxiaobai <sikuan.gu@gmail.com>
2015-06-04 13:40:23 +0800

    add root page

--- Gemfile
+++ Gemfile
@@ -23,6 +23,10 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+# A lightweight templating engine. Read more: http://slim-lang.com/
+gem 'slim-rails', '~> 3.0.1'
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ Bundler.require(*Rails.groups)
 
 module Orange
   class Application < Rails::Application
+    # Customizing Generators
+    config.generators.assets = false
+    config.generators.helper = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
