0e1c679ae53cb274d05c857f86f38d826502aa43
guxiaobai <sikuan.gu@gmail.com>
2015-06-04 20:06:29 +0800

    模版根据路径选择

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  layout 'enterprise'
+  layout :main_layout
+
+  private
+    def main_layout
+      if ['/enterprises/sign_in'].include?( request.path )
+        false
+      elsif params[:controller] =~ /^enterprise/
+        'enterprise'
+      else
+        'application'
+      end
+    end
 end
